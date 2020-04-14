e16588f2cc2df546e60de14606122050665859fa
201710098095 <839812298@qq.com>
2020-04-14 16:35:13 +0800

    完成报表功能

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,8 @@ import Params from '.././components/goods/Params.vue'
 import GoodsList from '.././components/goods/List.vue'
 import Add from '.././components/goods/Add.vue'
 import Order from '.././components/order/Order.vue'
+import Report from '.././components/report/Report.vue'
+
 
 
 
@@ -46,6 +48,8 @@ const routes = [
     {path: '/goods',component:GoodsList},
     {path: '/goods/add',component:Add},
     {path: '/orders',component:Order},
+    {path: '/reports',component:Report},
+
 
   
   ] 
